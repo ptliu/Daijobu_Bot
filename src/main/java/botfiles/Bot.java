d26ead67014aeb18a5fbbef28788acbe6c01c4fd
ptliu <patrickliu2016@gmail.com>
2017-04-02 13:43:42 +0900

    gradle stuff
    gradle still doesn't work xd

--- src/main/java/botfiles/Bot.java
+++ src/main/java/botfiles/Bot.java
@@ -11,26 +11,9 @@ import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
-import sx.blah.discord.handle.impl.events.ReadyEvent;
-import sx.blah.discord.handle.obj.*;
 import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.handle.audio.*;
-import sx.blah.discord.handle.obj.IChannel;
-import sx.blah.discord.handle.obj.IGuild;
-import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.handle.obj.IVoiceChannel;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RateLimitException;
-import de.btobastian.sdcf4j.*;
-import de.btobastian.sdcf4j.handler.Discord4JHandler;
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.player.*;
+
+
 
 
 class BotListeners{
@@ -70,8 +53,14 @@ public class Bot{
 	
 
 	
-	public static void main(String args[]) throws DiscordException{
-		client = getClient(TOKEN, true);
+	public static void main(String args[]) {
+		
+		try {
+			client = getClient(TOKEN, true);
+		} catch (DiscordException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		EventDispatcher dispatcher = client.getDispatcher();
 		dispatcher.registerListener(new BotListeners());
 		
